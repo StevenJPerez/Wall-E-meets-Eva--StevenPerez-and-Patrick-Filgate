3a18fcca469c40e139a86ea0f79cd526cad861c2
Patrick1796 <149628124+Patrick1796@users.noreply.github.com>
2023-12-04 14:33:36 -0600

    Wall-E Enclosure Code VFinal

--- ProjectCode.ino
+++ ProjectCode.ino
@@ -1,20 +1,102 @@
-const int buttonPin = 8;
-int buttonVal;
+//By Patrick macartney-Filgate and Steven Perez
+#include <Servo.h>
+
+Servo myservoSpaceship;
+Servo myservoEva;
+Servo myservoRock;
+
+int pos = 0;    // variable to store the servo position
 
 void setup() {
-//put your code setup here, to run once;
-pinMode(buttonPin, INPUT);
+  myservoSpaceship.attach(2);  // attaches the servo on pin 9 to the servo object
+  myservoEva.attach(3);  // attaches the servo on pin 9 to the servo object
+  myservoRock.attach(4);  // attaches the servo on pin 9 to the servo object
+
+
+
+   for (pos = 60; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoSpaceship.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15 ms for the servo to reach the position
+  }
+
+ for (pos = 90; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoEva.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
+
+   for (pos = 90; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoRock.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
 }
 
 void loop() {
-buttonVal = digitalRead(buttonPin);
-
-if (buttonVal == LOW) {
-//do this
-} else {
-// do that
+  // for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  //   // in steps of 1 degree
+  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  //   delay(15);                       // waits 15 ms for the servo to reach the position
+  // }
+  // for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  //   myservo.write(pos);              // tell servo to go to position in variable 'pos'
+  //   delay(15);                       // waits 15 ms for the servo to reach the position
+  // }
 }
-}
 
-practice branch exercise!- Steven
-Practiceing the Branch Exercise -- Patrick.
+
+/*bak
+pattern 1
+
+  for (pos = 60; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoSpaceship.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15 ms for the servo to reach the position
+  }
+
+ for (pos = 90; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoEva.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
+
+   for (pos = 90; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoRock.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
+
+
+
+
+pattern 2
+
+
+ for (pos = 0; pos <= 60; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoSpaceship.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(30);                       // waits 15 ms for the servo to reach the position
+  }
+
+ for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoEva.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
+
+ for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservoRock.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+
+
+
+  }
+  */
